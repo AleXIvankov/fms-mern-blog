333a727a91713d33acba8b96d051147907c175a6
Alex <alekseiivankov8787@gmail.com>
2024-02-18 15:40:53 +0100

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,26 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3002;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@fms-blog.0s55xvi.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("mongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Server started on port 300");
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
 });
