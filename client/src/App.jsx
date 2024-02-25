2214ebe18b187fab20c64c84f7bdac8bee96fa3e
Alex <alekseiivankov8787@gmail.com>
2024-02-25 11:51:03 +0100

    create the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Preventif from "./pages/Preventif";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Footer } from "flowbite-react";
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-4 border-indigo-500/50">
+      <div className="w-full max-w-7xl mx-auto">
+        <Footer.LinkGroup col className="text-sky-600 ">
+          <Footer.Link
+            href="/"
+            target="_blanc"
+            rel="noopener noreferrer"
+            className=" hover:text-sky-800 no-underline hover:underline"
+          >
+            Retour
+          </Footer.Link>
+          <Footer.Link
+            href="/"
+            target="_blanc"
+            rel="noopener noreferrer"
+            className=" hover:text-sky-800 no-underline hover:underline"
+          >
+            Contacts
+          </Footer.Link>
+        </Footer.LinkGroup>
+        <Footer.Divider />
+        <div>
+          <Footer.Copyright by="FMS blog" year={new Date().getFullYear()} />
+        </div>
+      </div>
+    </Footer>
+  );
+}
