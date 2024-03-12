44dfa78b4e2271884b05c87207e38e4a4a29c943
Alex <alekseiivankov8787@gmail.com>
2024-03-12 13:57:46 +0100

    add more functionality to the posts result of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   console.log(userPosts);
   useEffect(() => {
     const fetchPosts = async () => {
@@ -14,68 +15,102 @@ export default function DashPosts() {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.post.length < 9) {
+            setShowMore(false);
+          }
         }
-      } catch (error) {}
+      } catch (error) {
+        console.log(error.message);
+      }
     };
     if (currentUser.isAdmin) {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
-        <Table hoverable className="shadow-md">
-          <Table.Head>
-            <Table.HeadCell>Date de mise à jour</Table.HeadCell>
-            <Table.HeadCell>Image</Table.HeadCell>
-            <Table.HeadCell>Titre de l'article</Table.HeadCell>
-            <Table.HeadCell>Catégorie</Table.HeadCell>
-            <Table.HeadCell>Supprimer</Table.HeadCell>
-            <Table.HeadCell>
-              <span>Modifier</span>
-            </Table.HeadCell>
-          </Table.Head>
-          {userPosts.map((post) => (
-            <Table.Body className="divide-y">
-              <Table.Row className="bg-sky-100 dark:border-gray-700 dark:bg-gray-800">
-                <Table.Cell>
-                  {new Date(post.updatedAt).toLocaleDateString()}
-                </Table.Cell>
-                <Table.Cell>
-                  <Link to={`/post/${post.slug}`}>
-                    <img
-                      src={post.image}
-                      alt={post.title}
-                      className="w-20 h-10 object-cover bg-slate-500"
-                    />
-                  </Link>
-                </Table.Cell>
-                <Table.Cell>
-                  <Link
-                    className="font-light text-gray-800 dark:text-white text-medium"
-                    to={`/post/${post.slug}`}
-                  >
-                    {post.title}
-                  </Link>
-                </Table.Cell>
-                <Table.Cell>{post.category}</Table.Cell>
-                <Table.Cell>
-                  <span className="text-red-500 font-medium hover:underline cursor-pointer">
-                    Supprimer
-                  </span>
-                </Table.Cell>
-                <Table.Cell>
-                  <Link
-                    className="text-teal-500 hover:underline"
-                    to={`/update-post/${post._id}`}
-                  >
-                    <span>Modifier</span>
-                  </Link>
-                </Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          ))}
-        </Table>
+        <>
+          <Table hoverable className="shadow-md">
+            <Table.Head>
+              <Table.HeadCell>Date de mise à jour</Table.HeadCell>
+              <Table.HeadCell>Image</Table.HeadCell>
+              <Table.HeadCell>Titre de l'article</Table.HeadCell>
+              <Table.HeadCell>Catégorie</Table.HeadCell>
+              <Table.HeadCell>Supprimer</Table.HeadCell>
+              <Table.HeadCell>
+                <span>Modifier</span>
+              </Table.HeadCell>
+            </Table.Head>
+            {userPosts.map((post) => (
+              <Table.Body className="divide-y">
+                <Table.Row className="bg-sky-100 dark:border-gray-700 dark:bg-gray-800">
+                  <Table.Cell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        alt={post.title}
+                        className="w-20 h-10 object-cover bg-slate-500"
+                      />
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="font-light text-gray-800 dark:text-white text-medium"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </Table.Cell>
+                  <Table.Cell>{post.category}</Table.Cell>
+                  <Table.Cell>
+                    <span className="text-red-500 font-medium hover:underline cursor-pointer">
+                      Supprimer
+                    </span>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Link
+                      className="text-teal-500 hover:underline"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Modifier</span>
+                    </Link>
+                  </Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className="w-full text-blue-600 self-center text-sm py-5"
+            >
+              Afficher plus
+            </button>
+          )}
+        </>
       ) : (
         <p> Vous n'avez pas des posts</p>
       )}
